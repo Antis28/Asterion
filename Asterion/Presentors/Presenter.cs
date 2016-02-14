ce70c92f737941606e97a733156ef636ff348ef7
Antis <Antis28@mail.ru>
2016-02-14 19:25:43 +0300

    (Добавил фалы в репозиторий)

---
+++ Asterion/Presentors/Presenter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Asterion.Models;
+
+
+namespace Asterion.Presentors
+{
+    /// <summary>
+    /// MVP - model-view-presenter
+    /// Presenter служит для связывание логики и UI
+    /// </summary>
+    class Presenter
+    {
+        AlarmTimer alarmTimer = null;
+        MainWindow mainWindow = null;
+
+        public Presenter( MainWindow mainWindow )
+        {
+            this.alarmTimer = new AlarmTimer();
+            this.mainWindow = mainWindow;
+            this.mainWindow.startAlarmEvent += new EventHandler( mainWindow_startAlarmEvent );
+        }
+
+        void mainWindow_startAlarmEvent( object sender, System.EventArgs e )
+        {
+            //alarmTimer.Logic();
+
+            
+        }
+    }
+}
