16df6c062bd2ec5f234d44dfedde130b0a6be34f
Antis <Antis28@mail.ru>
2016-02-14 21:57:44 +0300

    Удалление файлов по списку готово.

--- Asterion/MainWindow.xaml.cs
+++ Asterion/MainWindow.xaml.cs
@@ -10,6 +10,9 @@ namespace Asterion
 
     public partial class MainWindow : Window
     {
+
+        System.Windows.Threading.DispatcherTimer timerDiskSpace = new System.Windows.Threading.DispatcherTimer();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -18,7 +21,7 @@ namespace Asterion
             new PresenterClean( this );
         }
 
-        public event EventHandler startAlarmEvent = null;        
+        public event EventHandler startAlarmEvent = null;
 
         private void startAlarm_Click( object sender, RoutedEventArgs e )
         {
@@ -38,9 +41,31 @@ namespace Asterion
         }
 
         public event EventHandler CleanEvent = null;
-        //private void previewCleanButton_Click( object sender, RoutedEventArgs e )
-        //{
-        //    previewCleanEvent.Invoke( sender, e );
-        //}
+
+        private void cleanButton_Click( object sender, RoutedEventArgs e )
+        {
+            CleanEvent.Invoke( sender, e );
+        }
+
+        private void comboBox_Initialized( object sender, EventArgs e )
+        {
+            for( int i = 1; i < 5; i++ )
+            {
+                comboBox.Items.Add( i );
+            }
+            comboBox.SelectedIndex = 0;
+        }
+
+        public event EventHandler checkBoxCleanEvent = null;
+        private void checkBox_Click( object sender, RoutedEventArgs e )
+        {
+            checkBoxCleanEvent.Invoke( sender, e );
+        }
+        public event EventHandler sizeDiskProgressBarEvent = null;
+        private void sizeDiskProgressBar_Initialized( object sender, EventArgs e )
+        {
+            new SpaceDiskInPercent(this);
+            sizeDiskProgressBarEvent.Invoke( sender, e );
+        }
     }
 }
