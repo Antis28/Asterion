29401bd2d17c441925732f32ec7ccb0bd522e557
Antis <Antis28@mail.ru>
2016-02-14 19:58:23 +0300

    Добавляю файлы логики и презентера в репозиторий.

---
+++ Asterion/Presentors/PresenterAlarmTimer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Asterion.Models;
+
+
+namespace Asterion.Presentors
+{
+    /// <summary>
+    /// MVP - model-view-presenter
+    /// Presenter служит для связывание логики и UI
+    /// </summary>
+    class PresenterAlarmTimer
+    {
+        LogicAlarmTimer alarmTimer = null;
+        MainWindow mainWindow = null;
+
+        public PresenterAlarmTimer( MainWindow mainWindow )
+        {
+            this.alarmTimer = new LogicAlarmTimer();
+            this.mainWindow = mainWindow;
+            this.mainWindow.startAlarmEvent += new EventHandler( mainWindow_startAlarmEvent );
+        }
+
+        void mainWindow_startAlarmEvent( object sender, System.EventArgs e )
+        {
+        }
+    }
+}
---
+++ Asterion/Presentors/PresenterRestartProcess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Asterion.Models;
+
+namespace Asterion.Presentors
+{
+    class PresenterRestartProcess
+    {
+        LogicRestartProcess alarmTimer = null;
+        MainWindow mainWindow = null;
+
+        public PresenterRestartProcess( MainWindow mainWindow )
+        {
+            this.alarmTimer = new LogicRestartProcess();
+            this.mainWindow = mainWindow;
+            this.mainWindow.restartProcessEvent += new EventHandler( mainWindow_restartProcessEvent );
+        }
+
+        void mainWindow_restartProcessEvent( object sender, System.EventArgs e )
+        {    
+        }
+    }
+}
