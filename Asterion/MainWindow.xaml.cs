00ca9153783f71b9e5fdccb9f5b25b4383ef6abb
antis <antis28@mail.ru>
2017-03-27 19:36:00 +0300

    add(ChellForWebP) Drag and Drop для адреса каталога

--- Asterion/MainWindow.xaml.cs
+++ Asterion/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Windows.Forms;
 using Asterion.Presentors;
 using Asterion.WpfExtensions;
 using System.Windows.Interop;
+using System.IO;
 
 namespace Asterion
 {
@@ -19,44 +20,44 @@ namespace Asterion
         public MainWindow()
         {
             InitializeComponent();
-            new PresenterAlarmTimer( this );
-            new PresenterRestartProcess( this );
-            new PresenterClean( this );
-            new PresenterFindExt( this );
-            new PresenterRenamer( this );
-            new PresenterFindExtInXMLBaseDate( this );
-            presenterChellForWebp = new PresenterChellForWebp( this );
+            new PresenterAlarmTimer(this);
+            new PresenterRestartProcess(this);
+            new PresenterClean(this);
+            new PresenterFindExt(this);
+            new PresenterRenamer(this);
+            new PresenterFindExtInXMLBaseDate(this);
+            presenterChellForWebp = new PresenterChellForWebp(this);
         }
 
         public event EventHandler startAlarmEvent = null;
         private void startAlarm_Click( object sender, RoutedEventArgs e )
         {
-            startAlarmEvent.Invoke( sender, e );
+            startAlarmEvent.Invoke(sender, e);
         }
 
         public event EventHandler restartProcessEvent = null;
         private void buttonRestartProcess_Click( object sender, RoutedEventArgs e )
         {
-            restartProcessEvent.Invoke( sender, e );
+            restartProcessEvent.Invoke(sender, e);
         }
 
         public event EventHandler previewCleanEvent = null;
         private void previewCleanButton_Click( object sender, RoutedEventArgs e )
         {
-            previewCleanEvent.Invoke( sender, e );
+            previewCleanEvent.Invoke(sender, e);
         }
 
         public event EventHandler CleanEvent = null;
         private void cleanButton_Click( object sender, RoutedEventArgs e )
         {
-            CleanEvent.Invoke( sender, e );
+            CleanEvent.Invoke(sender, e);
         }
 
         private void comboBox_Initialized( object sender, EventArgs e )
         {
             for( int i = 1; i < 5; i++ )
             {
-                comboBox.Items.Add( i );
+                comboBox.Items.Add(i);
             }
             comboBox.SelectedIndex = 0;
         }
@@ -64,48 +65,48 @@ namespace Asterion
         public event EventHandler checkBoxCleanEvent = null;
         private void checkBox_Click( object sender, RoutedEventArgs e )
         {
-            checkBoxCleanEvent.Invoke( sender, e );
+            checkBoxCleanEvent.Invoke(sender, e);
         }
         public event EventHandler sizeDiskProgressBarEvent = null;
         private void sizeDiskProgressBar_Initialized( object sender, EventArgs e )
         {
-            new SpaceDiskInPercent( this );
-            sizeDiskProgressBarEvent.Invoke( sender, e );
+            new SpaceDiskInPercent(this);
+            sizeDiskProgressBarEvent.Invoke(sender, e);
         }
         public event EventHandler findExtInBaseDateEvent = null;
-        public event EventHandler FindExtInXMLBaseDateEvent= null;
+        public event EventHandler FindExtInXMLBaseDateEvent = null;
         private void startSerach_Click( object sender, RoutedEventArgs e )
         {
             if( isSearchXML.IsChecked != true )
-                findExtInBaseDateEvent.Invoke( sender, e );
+                findExtInBaseDateEvent.Invoke(sender, e);
             else
-                FindExtInXMLBaseDateEvent.Invoke( sender, e );
+                FindExtInXMLBaseDateEvent.Invoke(sender, e);
         }
 
         public event EventHandler openFileDialogRenamerEvent = null;
         private void buttonOpenFileDialog_Click( object sender, RoutedEventArgs e )
         {
-            openFileDialogRenamerEvent.Invoke( sender, e );
+            openFileDialogRenamerEvent.Invoke(sender, e);
         }
 
         public event EventHandler startTimerEvent = null;
         private void startTimer_Click( object sender, RoutedEventArgs e )
         {
-            startTimerEvent.Invoke( sender, e );
+            startTimerEvent.Invoke(sender, e);
         }
 
 
         public event EventHandler logicRanamerEvent = null;
         private void buttonRenameFiles_Click( object sender, RoutedEventArgs e )
         {
-            logicRanamerEvent.Invoke( sender, e );
+            logicRanamerEvent.Invoke(sender, e);
         }
 
         private void hoursComboBox_Initialized( object sender, EventArgs e )
         {
             for( int i = 0; i < 24; i++ )
             {
-                hoursComboBox.Items.Add( i );
+                hoursComboBox.Items.Add(i);
             }
             hoursComboBox.SelectedIndex = 0;
         }
@@ -114,7 +115,7 @@ namespace Asterion
         {
             for( int i = 0; i < 60; i++ )
             {
-                minutesComboBox.Items.Add( i );
+                minutesComboBox.Items.Add(i);
             }
             minutesComboBox.SelectedIndex = 0;
         }
@@ -122,19 +123,19 @@ namespace Asterion
         public event EventHandler openFileDialogEvent = null;
         private void openFileDialogToAlarmAndTimer_Click( object sender, RoutedEventArgs e )
         {
-            openFileDialogEvent.Invoke( sender, e );
+            openFileDialogEvent.Invoke(sender, e);
         }
 
 
         public event EventHandler logOutTrackEvent = null;
         protected override void OnSourceInitialized( EventArgs e )
         {
-            base.OnSourceInitialized( e );
+            base.OnSourceInitialized(e);
             // создаем экземпляр HwndSource
             Asterion.Models.LogicLogOutTrack LOT = new Asterion.Models.LogicLogOutTrack();
-            LOT.hwndSource = PresentationSource.FromVisual( this ) as HwndSource;
+            LOT.hwndSource = PresentationSource.FromVisual(this) as HwndSource;
             // и устанавливаем перехватчик
-            LOT.hwndSource.AddHook( LOT.WndProc );
+            LOT.hwndSource.AddHook(LOT.WndProc);
             // logOutTrackEvent - ?
             if( e.ToString() == "" )
                 logOutTrackEvent.Invoke(this, e);
@@ -146,19 +147,19 @@ namespace Asterion
         private void btn_addAddress_Click( object sender, RoutedEventArgs e )
         {
             if( cb_isDirectory.IsChecked == true )
-                openFolderDialogEvent.Invoke( sender, e );
+                openFolderDialogEvent.Invoke(sender, e);
             else
-                openFileDialogToConverterEvent.Invoke( sender, e );
+                openFileDialogToConverterEvent.Invoke(sender, e);
         }
-        public event EventHandler startConvertEvent= null;
+        public event EventHandler startConvertEvent = null;
         private void btn_convert_Click( object sender, RoutedEventArgs e )
         {
-            startConvertEvent.Invoke( sender, e );
+            startConvertEvent.Invoke(sender, e);
         }
 
         private void tb_addressField_DragEnter( object sender, System.Windows.DragEventArgs e )
         {
-            System.Windows.MessageBox.Show( e.ToString() );
+            System.Windows.MessageBox.Show(e.ToString());
         }
 
         private void tb_addressField_LostFocus( object sender, RoutedEventArgs e )
@@ -170,6 +171,60 @@ namespace Asterion
         private void pb_percentConvert_MouseUp( object sender, System.Windows.Input.MouseButtonEventArgs e )
         {
             isPercent = !isPercent;
+        }
+
+        private void text_PreviewDragEnter( object sender, System.Windows.DragEventArgs e )
+        {
+            bool isCorrect = true;
+
+            if( e.Data.GetDataPresent(System.Windows.DataFormats.FileDrop, true) == true )
+            {
+                string[] filenames = (string[])e.Data.GetData(System.Windows.DataFormats.FileDrop, true);
+
+                foreach( string filename in filenames )
+                {
+                    if( Path.GetExtension(filename).Length == 0 )
+                    {
+
+                        isCorrect = true;
+                        break;
+
+                    }
+                    if( File.Exists(filename) == false )
+                    {
+                        isCorrect = false;
+                        break;
+                    }
+                    FileInfo info = new FileInfo(filename);
+                    if( info.Extension.ToLower() != ".txt" &&
+                        info.Extension.ToLower() != ".png" &&
+                        info.Extension.ToLower() != ".jpg" &&
+                        info.Extension.ToLower() != ".jpeg" &&
+                        info.Extension.ToLower() != ".tiff"
+                        )
+                    {
+                        isCorrect = false;
+                        break;
+                    }
+                }
+            }
+            if( isCorrect == true )
+                e.Effects = System.Windows.DragDropEffects.All;
+            else
+                e.Effects = System.Windows.DragDropEffects.None;
+            e.Handled = true;
+        }
+
+        private void text_PreviewDrop( object sender, System.Windows.DragEventArgs e )
+        {
+            tb_addressField.Text = string.Empty;
+            string[] filenames = (string[])e.Data.GetData(System.Windows.DataFormats.FileDrop, true);
+            foreach( string filename in filenames )
+                if( Path.GetExtension(filename).Length != 0 )
+                    tb_addressField.Text = Path.GetDirectoryName(filename);
+                else
+                    tb_addressField.Text = filename;
+            e.Handled = true;
         }
     }
 }
