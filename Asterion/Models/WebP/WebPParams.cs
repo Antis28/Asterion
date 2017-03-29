a9b71bbcf9e3515f19556c5104878b44705438cd
antis <antis28@mail.ru>
2017-03-29 21:30:48 +0300

    add(WebPParams) профиль для конвертации

--- Asterion/Models/WebP/WebPParams.cs
+++ Asterion/Models/WebP/WebPParams.cs
@@ -31,6 +31,9 @@ namespace Asterion.Models.WebP
             }
         }
 
+        public enum Profile : int { Custom, Default, Photo, Picture, Drawing, Icon, Text }
+
+        public Profile profile = Profile.Default;
         // Качество изображения
         public int quality = 85;
         public int qualityAlpha = 100;
@@ -94,7 +97,7 @@ namespace Asterion.Models.WebP
         public float PSNR
         {
             set
-            {                
+            {
                 if( value >= 0 && value <= 100 )
                 {
                     psnr = value;
@@ -162,91 +165,116 @@ namespace Asterion.Models.WebP
         private bool IsFilterStrength = false;
         private float filterStrength = 0;
 
-
-
-
-
+        /// <summary>
+        /// Параметры для Webp конвертера
+        /// </summary>
+        /// <param name="pathDirectory"></param>
+        /// <returns></returns>
         public string BuildParams( string pathDirectory )
-        {
-            // Параметры для Webp конвертера
+        {            
             StringBuilder sb = new StringBuilder();
-            if( resolution.IsValid() )
+            
+            if( profile == Profile.Custom)
             {
-                sb.Append("-resize ");              // -resize
-                sb.Append(resolution.ToString());   // <w> <h>
+                #region Profile.Custom
+                if( resolution.IsValid() )
+                {
+                    sb.Append("-resize ");              // -resize
+                    sb.Append(resolution.ToString());   // <w> <h>
+                }
+                if( IsProgress )
+                    sb.Append(" -progress ");   // -progress  report encoding progress 
+                if( IsVerbose )
+                    sb.Append(" -v ");      // -v       verbose, e.g. print encoding/decoding times
+                
+                if( IsShort )
+                    sb.Append(" -short "); // -short ................. condense printed message
+
+                //-metadata <string> ..... comma separated list of metadata to
+                //copy from the input to the output if present.
+                //Valid values: all, none(default), exif, icc, xmp
+                if( IsMetadataCopy )
+                    sb.Append(" -metadata all ");
+
+                //-lossless..............encode image losslessly
+                if( IsLossless )
+                    sb.Append(" -lossless ");
+                //-noalpha...............discard any transparency information
+                if( IsNoalpha )
+                    sb.Append(" -noalpha ");
+                //-alpha_cleanup.........clean RGB values in transparent area
+                if( IsAlpha_cleanup )
+                    sb.Append(" -alpha_cleanup ");
+
+                //-m<int>...............compression method(0 = fast, 6 = slowest)
+                if( IsCompressionMethod )
+                {
+                    sb.Append(" -m ");
+                    sb.Append(Compression);
+                    sb.Append(" ");
+                }
+                //-psnr<float>..........target PSNR(in dB.typically: 42)Пиковое отношение сигнала к шуму
+                if( IsPSNR )
+                {
+                    sb.Append(" -psnr ");
+                    sb.Append(PSNR);
+                    sb.Append(" ");
+                }
+                //-sns<int>.............spatial noise shaping (0:off, 100:max)
+                if( IsSNS )
+                {
+                    sb.Append(" -sns ");
+                    sb.Append(SNS);
+                    sb.Append(" ");
+                }
+                //-f<int>...............filter strength(0 = off..100)
+                if( IsFilterStrength )
+                {
+                    sb.Append(" -f ");
+                    sb.Append(FilterStrength);
+                    sb.Append(" ");
+                }
+                sb.Append(" -alpha_q ");    // -alpha_q качество изображения для альфа канала от 0 до 100   
+                sb.Append(qualityAlpha);
+                #endregion
+            }            
+            else
+            {
+                #region Profile other
+                switch( profile)
+                {
+                    case Profile.Default:
+                        sb.Append("-preset default ");                      
+                        break;
+                    case Profile.Drawing:
+                        sb.Append("-preset drawing ");
+                        break;
+                    case Profile.Icon:
+                        sb.Append("-preset icon ");
+                        break;
+                    case Profile.Photo:
+                        sb.Append("-preset photo ");
+                        break;
+                    case Profile.Picture:
+                        sb.Append("-preset picture ");
+                        break;
+                    case Profile.Text:
+                        sb.Append("-preset text ");
+                        break;
+                }
+                #endregion
             }
-            if( IsProgress )
-                sb.Append(" -progress ");   // -progress  report encoding progress 
-            if( IsVerbose )
-                sb.Append(" -v ");      // -v       verbose, e.g. print encoding/decoding times
+
             if( IsQuiet )
                 sb.Append(" -quiet ");  //-quiet    don't print anything
-            if( IsShort )
-                sb.Append(" -short "); // -short ................. condense printed message
-
-            //-metadata <string> ..... comma separated list of metadata to
-            //copy from the input to the output if present.
-            //Valid values: all, none(default), exif, icc, xmp
-            if( IsMetadataCopy )
-                sb.Append(" -metadata all ");
-
-            //-lossless..............encode image losslessly
-            if( IsLossless )
-                sb.Append(" -lossless ");
-            //-noalpha...............discard any transparency information
-            if( IsNoalpha )
-                sb.Append(" -noalpha ");
-            //-alpha_cleanup.........clean RGB values in transparent area
-            if( IsAlpha_cleanup )
-                sb.Append(" -alpha_cleanup ");
-
-            //-m<int>...............compression method(0 = fast, 6 = slowest)
-            if( IsCompressionMethod )
-            {
-                sb.Append(" -m ");
-                sb.Append(Compression);
-                sb.Append(" ");
-            }
-            //-psnr<float>..........target PSNR(in dB.typically: 42)Пиковое отношение сигнала к шуму
-            if( IsPSNR )
-            {
-                sb.Append(" -psnr ");
-                sb.Append(PSNR);
-                sb.Append(" ");
-            }
-            //-sns<int>.............spatial noise shaping (0:off, 100:max)
-            if( IsSNS )
-            {
-                sb.Append(" -sns ");
-                sb.Append(SNS);
-                sb.Append(" ");
-            }
-            //-f<int>...............filter strength(0 = off..100)
-            if( IsFilterStrength )
-            {
-                sb.Append(" -f ");
-                sb.Append(FilterStrength);
-                sb.Append(" ");
-            }
-
 
             sb.Append(" -q ");          // -q       качество изображения от 0 до 100
             sb.Append(quality);
-            sb.Append(" -alpha_q ");    // -alpha_q качество изображения для альфа канала от 0 до 100   
-            sb.Append(qualityAlpha);
-            sb.Append(" -o ");          // -o       адрес вывода файла
-            sb.Append("\"");
+            
+            sb.Append(" -o \"");          // -o       адрес вывода файла
             sb.Append(pathDirectory);
             sb.Append(@"\output\");     //          каталог вывода
 
-            //commandParameters = string.Format(" -q {0} -alpha_q {1} {4} -o \"{2}{3}",
-            //        quality,            //{0} -q       качество изображения от 0 до 100
-            //        qualityAlpha,       //{1} -alpha_q качество изображения для альфа канала от 0 до 100
-            //        pathDirectory,      //{2}  -o      адрес вывода файла
-            //        @"\output\",        //{3}          каталог вывода
-            //       "-resize " +
-            //       resolution.ToString()//{4} -resize
-            //    );
             return sb.ToString();
         }
     }
