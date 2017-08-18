1d800c4d3b513c18194a67dea470714891e5f940
antis <antis28@mail.ru>
2017-08-19 01:17:38 +0300

    add(srtToTxt) конвертация в сплошной текст и обратно
    конвертация обратно - по исходному файлу

--- Asterion/ConvertSRTtoTXT/ModelSRTtoTXT.cs
+++ Asterion/ConvertSRTtoTXT/ModelSRTtoTXT.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 
 namespace Asterion.ConvertSRTtoTXT
@@ -55,10 +56,127 @@ namespace Asterion.ConvertSRTtoTXT
         private void Start()
         {
             ExtractPathsFiles(pathDirectory); // получить адреса файлов
-
-            StreamReader sReader = new StreamReader(pathToInputFiles[0]);
+            string path = pathToInputFiles[0];
+            string text = ReadFromSrtFile(path);
+            WriteToTextFile(path, text);
+            WriteToSrtFile(path);
 
         }
+
+        private void WriteToSrtFile( string writePath )
+        {
+            string readPath = string.Empty;
+            string text = string.Empty;
+            string fileName = string.Empty;
+            string FullFileName = string.Empty;
+            string folderName = string.Empty;
+
+            readPath = Path.GetFileNameWithoutExtension(writePath) + ".txt";
+            fileName = Path.GetFileNameWithoutExtension(writePath);
+            folderName = Path.GetDirectoryName(writePath);
+            FullFileName = folderName + "\\" + fileName + "-ru.srt";
+            int counter = 1;
+
+            Regex rxTime = new Regex(@"\d+:\d+"); // 00:00
+            Regex rxNum = new Regex(@"^\d+"); // 00
+            try
+            {   // Open the text file using a stream reader.
+                using( StreamWriter swResult = new StreamWriter(File.Create(FullFileName), System.Text.Encoding.UTF8) )
+                {
+                    using( StreamReader srOriginal = new StreamReader(writePath, System.Text.Encoding.UTF8) )
+                    {
+                        using( StreamReader srSource = new StreamReader(readPath, System.Text.Encoding.UTF8) )
+                        {
+                            while( true )
+                            {
+                                // Читаем строку из файла во временную переменную.
+                                string temp = srOriginal.ReadLine();
+
+                                // Если достигнут конец файла, прерываем считывание.
+                                if( temp == null )
+                                    break;
+                                if( temp == string.Empty || rxNum.IsMatch(temp) )
+                                {
+                                    swResult.WriteLine(temp);
+                                    continue;
+                                }
+
+                                if( rxTime.IsMatch(temp) )
+                                {
+                                    //swResult.WriteLine(counter++);
+                                    //swResult.WriteLine();
+                                    swResult.WriteLine(temp);
+                                    swResult.WriteLine();
+                                    swResult.WriteLine(srSource.ReadLine());
+                                    //swResult.WriteLine();
+                                }
+                                else
+                                {
+                                    swResult.WriteLine(srSource.ReadLine());
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch( Exception e )
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private void WriteToTextFile( string writePath, string text )
+        {
+            writePath = Path.GetFileNameWithoutExtension(writePath) + ".txt";
+            try
+            {
+                using( StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.UTF8) )
+                {
+                    sw.WriteLine(text);
+                }
+            } catch( Exception e )
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private string ReadFromSrtFile( string readPath )
+        {
+            string text = "";
+            Regex rxTime = new Regex(@"^\d+:\d+"); // 00:00
+            Regex rxNum = new Regex(@"^\d+"); // 00
+            try
+            {   // Open the text file using a stream reader.
+                using( StreamReader sr = new StreamReader(readPath) )
+                {
+                    while( true )
+                    {
+                        // Читаем строку из файла во временную переменную.
+                        string temp = sr.ReadLine();
+                        // Если достигнут конец файла, прерываем считывание.
+                        if( temp == null )
+                            break;
+
+                        if( temp == string.Empty || rxNum.IsMatch(temp) )
+                            continue;
+
+                        bool isDigit = rxTime.IsMatch(temp);
+
+                        if( !isDigit )
+                        {
+                            // Пишем считанную строку в итоговую переменную.
+                            text += temp + "\n";
+                        }
+                    }
+
+                }
+            } catch( Exception e )
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+            return text;
+        }
+
         /// <summary>
         /// Получение полных путей файлов
         /// </summary>
@@ -79,18 +197,18 @@ namespace Asterion.ConvertSRTtoTXT
 
         public string[] FilterExtension( string pathDirectory )
         {
-        #if framewok_4_0
-                    //For .NET 4.0 and later, 
-                    var files = Directory.EnumerateFiles(pathDirectory, "*.*", SearchOption.TopDirectoryOnly)
-                    .Where(s =>
-                    s.EndsWith(".SRT", StringComparison.OrdinalIgnoreCase)
-                    );
-        #endif
-        #if framewok_do_4_0
+#if framewok_4_0
+            //For .NET 4.0 and later, 
+            var files = Directory.EnumerateFiles(pathDirectory, "*.*", SearchOption.TopDirectoryOnly)
+            .Where(s =>
+            s.EndsWith(".SRT", StringComparison.OrdinalIgnoreCase)
+            );
+#endif
+#if framewok_do_4_0
                     //For earlier versions of .NET,
                     var files = Directory.GetFiles("C:\\path", "*.*", SearchOption.AllDirectories)
                     .Where(s => s.EndsWith(".srt"));
-        #endif
+#endif
             return files.ToArray<string>();
         }
     }
